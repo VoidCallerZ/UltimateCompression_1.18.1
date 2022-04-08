a5645b176cfa2d2e3e8ed747545275b3391fd24b
VoidCallerZ <rick.rasenberg@xs4all.nl>
2022-04-08 16:15:33 +0200

    Oregeneration is working, config file is confusing

--- src/main/java/com/VoidCallerZ/uc/worldgen/ores/Ores.java
+++ src/main/java/com/VoidCallerZ/uc/worldgen/ores/Ores.java
@@ -2,11 +2,8 @@ package com.VoidCallerZ.uc.worldgen.ores;
 
 import com.VoidCallerZ.uc.setup.registration.Registration;
 import net.minecraft.core.Holder;
-import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.animal.Panda;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.GenerationStep;
@@ -20,37 +17,59 @@ import net.minecraftforge.event.world.BiomeLoadingEvent;
 
 public class Ores
 {
-    public static Holder<PlacedFeature> UC_WORLDGEN;
+    public static Holder<PlacedFeature> IRON_OREGEN;
+    public static Holder<PlacedFeature> GOLD_OREGEN;
+    public static Holder<PlacedFeature> COPPER_OREGEN;
+    public static Holder<PlacedFeature> DIAMOND_OREGEN;
+    public static Holder<PlacedFeature> COAL_OREGEN;
+    public static Holder<PlacedFeature> EMERALD_OREGEN;
+    public static Holder<PlacedFeature> LAPIS_OREGEN;
+    public static Holder<PlacedFeature> REDSTONE_OREGEN;
     public static Holder<PlacedFeature> UC_DEEPSLATEGEN;
     public static Holder<PlacedFeature> UC_NETHERGEN;
 
     public static void registerConfiguredFeatures()
     {
-        //Overworld
-        //Iron
-        OverworldOreGen("compressed_iron_ore", Registration.COMPRESSED_IRON_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 48);
-        DeepslateOreGen("compressed_deepslate_iron_ore", Registration.COMPRESSED_DEEPSLATE_IRON_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
-        //Gold
-        OverworldOreGen("compressed_gold_ore", Registration.COMPRESSED_GOLD_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 48);
-        DeepslateOreGen("compressed_deepslate_gold_ore", Registration.COMPRESSED_DEEPSLATE_GOLD_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
-        //Copper
-        OverworldOreGen("compressed_copper_ore", Registration.COMPRESSED_COPPER_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 96);
-        DeepslateOreGen("compressed_deepslate_copper_ore", Registration.COMPRESSED_DEEPSLATE_COPPER_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(),-16, 0);
-        //Diamond
-        OverworldOreGen("compressed_diamond_ore", Registration.COMPRESSED_DIAMOND_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_DIAMOND_VEINSIZE.get(), OresConfig.OVERWORLD_DIAMOND_AMOUNT.get(), 0, 16);
-        DeepslateOreGen("compressed_deepslate_diamond_ore", Registration.COMPRESSED_DEEPSLATE_DIAMOND_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_DIAMOND_VEINSIZE.get(), OresConfig.DEEPSLATE_DIAMOND_AMOUNT.get(), -64, 0);
-        //Coal
-        OverworldOreGen("compressed_coal_ore", Registration.COMPRESSED_COAL_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 256);
-        DeepslateOreGen("compressed_deepslate_coal_ore", Registration.COMPRESSED_DEEPSLATE_COAL_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -16, 0);
-        //Emerald
-        OverworldOreGen("compressed_emerald_ore", Registration.COMPRESSED_EMERALD_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 16);
-        DeepslateOreGen("compressed_deepslate_emerald_ore", Registration.COMPRESSED_DEEPSLATE_EMERALD_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
-        //Lapis
-        OverworldOreGen("compressed_lapis_ore", Registration.COMPRESSED_LAPIS_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 64);
-        DeepslateOreGen("compressed_deepslate_lapis_ore", Registration.COMPRESSED_DEEPSLATE_LAPIS_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
-        //Redstone
-        OverworldOreGen("compressed_redstone_ore", Registration.COMPRESSED_REDSTONE_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 16);
-        DeepslateOreGen("compressed_deepslate_redstone_ore", Registration.COMPRESSED_DEEPSLATE_REDSTONE_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
+        //Overworld Ores
+        IRON_OREGEN = registerPlacedFeatureUniform("compressed_iron_ore", Registration.COMPRESSED_IRON_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 48);
+        GOLD_OREGEN = registerPlacedFeatureUniform("compressed_gold_ore", Registration.COMPRESSED_GOLD_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 48);
+        COPPER_OREGEN = registerPlacedFeatureUniform("compressed_copper_ore", Registration.COMPRESSED_COPPER_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 96);
+        DIAMOND_OREGEN = registerPlacedFeatureUniform("compressed_diamond_ore", Registration.COMPRESSED_DIAMOND_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 16);
+        COAL_OREGEN = registerPlacedFeatureUniform("compressed_coal_ore", Registration.COMPRESSED_COAL_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_COAL_VEINSIZE.get(), OresConfig.OVERWORLD_COAL_AMOUNT.get(), 136, 255);
+        EMERALD_OREGEN = registerPlacedFeatureTriangle("compressed_emerald_ore", Registration.COMPRESSED_EMERALD_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 255);
+        LAPIS_OREGEN = registerPlacedFeatureUniform("compressed_lapis_ore", Registration.COMPRESSED_LAPIS_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 30);
+        REDSTONE_OREGEN = registerPlacedFeatureUniform("compressed_redstone_ore", Registration.COMPRESSED_REDSTONE_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 15);
+//        UC_WORLDGEN = registerPlacedFeature("compressed_coal_ore", Registration.COMPRESSED_COAL_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 256);
+//        UC_WORLDGEN = registerPlacedFeature("compressed_emerald_ore", Registration.COMPRESSED_EMERALD_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 16);
+//        UC_WORLDGEN = registerPlacedFeature("compressed_lapis_ore", Registration.COMPRESSED_LAPIS_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 64);
+//        UC_WORLDGEN = registerPlacedFeature("compressed_redstone_ore", Registration.COMPRESSED_REDSTONE_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 16);
+
+        //Deepslate Ores
+//        //Overworld
+//        //Iron
+//        OverworldOreGen("compressed_iron_ore", Registration.COMPRESSED_IRON_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 48);
+//        DeepslateOreGen("compressed_deepslate_iron_ore", Registration.COMPRESSED_DEEPSLATE_IRON_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
+//        //Gold
+//        OverworldOreGen("compressed_gold_ore", Registration.COMPRESSED_GOLD_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 48);
+//        DeepslateOreGen("compressed_deepslate_gold_ore", Registration.COMPRESSED_DEEPSLATE_GOLD_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
+//        //Copper
+//        OverworldOreGen("compressed_copper_ore", Registration.COMPRESSED_COPPER_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 96);
+//        DeepslateOreGen("compressed_deepslate_copper_ore", Registration.COMPRESSED_DEEPSLATE_COPPER_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(),-16, 0);
+//        //Diamond
+//        OverworldOreGen("compressed_diamond_ore", Registration.COMPRESSED_DIAMOND_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_DIAMOND_VEINSIZE.get(), OresConfig.OVERWORLD_DIAMOND_AMOUNT.get(), 0, 16);
+//        DeepslateOreGen("compressed_deepslate_diamond_ore", Registration.COMPRESSED_DEEPSLATE_DIAMOND_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_DIAMOND_VEINSIZE.get(), OresConfig.DEEPSLATE_DIAMOND_AMOUNT.get(), -64, 0);
+//        //Coal
+//        OverworldOreGen("compressed_coal_ore", Registration.COMPRESSED_COAL_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 256);
+//        DeepslateOreGen("compressed_deepslate_coal_ore", Registration.COMPRESSED_DEEPSLATE_COAL_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -16, 0);
+//        //Emerald
+//        OverworldOreGen("compressed_emerald_ore", Registration.COMPRESSED_EMERALD_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 16);
+//        DeepslateOreGen("compressed_deepslate_emerald_ore", Registration.COMPRESSED_DEEPSLATE_EMERALD_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
+//        //Lapis
+//        OverworldOreGen("compressed_lapis_ore", Registration.COMPRESSED_LAPIS_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 64);
+//        DeepslateOreGen("compressed_deepslate_lapis_ore", Registration.COMPRESSED_DEEPSLATE_LAPIS_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
+//        //Redstone
+//        OverworldOreGen("compressed_redstone_ore", Registration.COMPRESSED_REDSTONE_ORE.get().defaultBlockState(), OresConfig.OVERWORLD_VEINSIZE.get(), OresConfig.OVERWORLD_AMOUNT.get(), 0, 16);
+//        DeepslateOreGen("compressed_deepslate_redstone_ore", Registration.COMPRESSED_DEEPSLATE_REDSTONE_ORE.get().defaultBlockState(), OresConfig.DEEPSLATE_VEINSIZE.get(), OresConfig.DEEPSLATE_AMOUNT.get(), -64, 0);
 
         //Nether
         //NetherOreGen("compressed_nether_gold_ore", Registration.COMPRESSED_NETHER_GOLD_ORE.get().defaultBlockState(), OresConfig.NETHER_GOLD_ORE_VEINSIZE.get(), OresConfig.NETHER_GOLD_ORE_AMOUNT.get(), 10, 117);
@@ -58,41 +77,39 @@ public class Ores
         //NetherOreGen("compressed_nether_quartz_ore", Registration.COMPRESSED_NETHER_QUARTZ_ORE.get().defaultBlockState(), OresConfig.NETHER_QUARTZ_ORE_VEINSIZE.get(), OresConfig.NETHER_QUARTZ_ORE_AMOUNT.get(), 10, 117);
     }
 
-    private static <C extends FeatureConfiguration, F extends Feature<C>> Holder<PlacedFeature> registerPlacedFeature(String registryName, ConfiguredFeature<C, F> feature, PlacementModifier... placementModifiers)
+    private static <C extends FeatureConfiguration, F extends Feature<C>> Holder<PlacedFeature> registerPlacedFeatureUniform(String registryName, BlockState oreBlock, int veinSize, int chunkAmount, int minY, int maxY)
     {
-        return PlacementUtils.register(registryName, Holder.direct(feature), placementModifiers);
+        return PlacementUtils.register(registryName, Holder.direct(new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, oreBlock, veinSize))),
+                CountPlacement.of(chunkAmount),
+                InSquarePlacement.spread(),
+                BiomeFilter.biome(),
+                HeightRangePlacement.uniform(VerticalAnchor.absolute(minY), VerticalAnchor.absolute(maxY)));
+    }
+
+    private static <C extends FeatureConfiguration, F extends Feature<C>> Holder<PlacedFeature> registerPlacedFeatureTriangle(String registryName, BlockState oreBlock, int veinSize, int chunkAmount, int minY, int maxY)
+    {
+        return PlacementUtils.register(registryName, Holder.direct(new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, oreBlock, veinSize))),
+                CountPlacement.of(chunkAmount),
+                InSquarePlacement.spread(),
+                BiomeFilter.biome(),
+                HeightRangePlacement.triangle(VerticalAnchor.absolute(minY), VerticalAnchor.absolute(maxY)));
     }
 
     public static void onBiomeLoadingEvent(BiomeLoadingEvent event)
     {
-        event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, UC_WORLDGEN);
-        //event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, UC_DEEPSLATEGEN);
-    }
-
-    private static void OverworldOreGen(String registryName, BlockState oreBlock, int veinSize, int chunkAmount, int minY, int maxY)
-    {
-        UC_WORLDGEN = registerPlacedFeature(registryName, new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OreFeatures.STONE_ORE_REPLACEABLES, oreBlock, veinSize)),
-                CountPlacement.of(chunkAmount),
-                InSquarePlacement.spread(),
-                BiomeFilter.biome(),
-                HeightRangePlacement.uniform(VerticalAnchor.absolute(minY), VerticalAnchor.absolute(maxY)));
-    }
-
-    private static void DeepslateOreGen(String registryName, BlockState oreBlock, int veinSize, int chunkAmount, int minY, int maxY)
-    {
-        UC_WORLDGEN = registerPlacedFeature(registryName, new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, oreBlock, veinSize)),
-                CountPlacement.of(chunkAmount),
-                InSquarePlacement.spread(),
-                BiomeFilter.biome(),
-                HeightRangePlacement.uniform(VerticalAnchor.absolute(minY), VerticalAnchor.absolute(maxY)));
-    }
-
-    private static void NetherOreGen(String registryName, BlockState oreBlock, int veinSize, int chunkAmount, int minY, int maxY)
-    {
-        UC_NETHERGEN = registerPlacedFeature(registryName, new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OreFeatures.NETHER_ORE_REPLACEABLES, oreBlock, veinSize)),
-                CountPlacement.of(chunkAmount),
-                InSquarePlacement.spread(),
-                BiomeFilter.biome(),
-                HeightRangePlacement.uniform(VerticalAnchor.absolute(minY), VerticalAnchor.absolute(maxY)));
+        if (event.getCategory() == Biome.BiomeCategory.MOUNTAIN)
+        {
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, EMERALD_OREGEN);
+        }
+        else
+        {
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, IRON_OREGEN);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, GOLD_OREGEN);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, COPPER_OREGEN);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, DIAMOND_OREGEN);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, COAL_OREGEN);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, LAPIS_OREGEN);
+            event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, REDSTONE_OREGEN);
+        }
     }
 }
--- src/main/java/com/VoidCallerZ/uc/worldgen/ores/OresConfig.java
+++ src/main/java/com/VoidCallerZ/uc/worldgen/ores/OresConfig.java
@@ -12,6 +12,11 @@ public class OresConfig
     public static ForgeConfigSpec.IntValue OVERWORLD_DIAMOND_AMOUNT;// = 3;
     public static ForgeConfigSpec.IntValue DEEPSLATE_DIAMOND_VEINSIZE;// = 3;
     public static ForgeConfigSpec.IntValue DEEPSLATE_DIAMOND_AMOUNT;// = 4;
+    public static ForgeConfigSpec.IntValue OVERWORLD_COAL_VEINSIZE;
+    public static ForgeConfigSpec.IntValue OVERWORLD_COAL_AMOUNT;
+    public static ForgeConfigSpec.IntValue DEEPSLATE_COAL_VEINSIZE;
+    public static ForgeConfigSpec.IntValue DEEPSLATE_COAL_AMOUNT;
+
 
     public static ForgeConfigSpec.IntValue NETHER_GOLD_ORE_VEINSIZE;
     public static ForgeConfigSpec.IntValue NETHER_GOLD_ORE_AMOUNT;
@@ -22,19 +27,24 @@ public class OresConfig
     {
         COMMON_BUILDER.comment("Settings for ore generation").push("ores");
 
-        OVERWORLD_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of all ores but diamond").defineInRange("overworldVeinsize", 5, 0, 50);
-        OVERWORLD_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk").defineInRange("overworldVeinsize", 3, 0, 50);
-        DEEPSLATE_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of all ores but diamond in deepslate").defineInRange("overworldVeinsize", 5, 0, 50);
-        DEEPSLATE_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk in deepslate").defineInRange("overworldVeinsize", 3, 0, 50);
+        OVERWORLD_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of iron, gold, copper, lapis & redstone").defineInRange("overworldVeinsize", 5, 0, 50);
+        OVERWORLD_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk").defineInRange("overworldChunkAmount", 5, 0, 50);
+        DEEPSLATE_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of iron, gold, copper, lapis & redstone").defineInRange("deepslateVeinsize", 5, 0, 50);
+        DEEPSLATE_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk in deepslate").defineInRange("deepslateChunkAmount", 5, 0, 50);
 
         OVERWORLD_DIAMOND_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of diamond ore").defineInRange("overworldDiamondVeinsize", 1, 0, 50);
-        OVERWORLD_DIAMOND_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk of diamond ore").defineInRange("overworldDiamondAmount", 3, 0, 50);
-        OVERWORLD_DIAMOND_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of diamond ore in deepslate").defineInRange("deepslateDiamondVeinsize", 3, 0, 50);
-        OVERWORLD_DIAMOND_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk of diamond ore in deepslate").defineInRange("deepslateDiamondAmount", 4, 0, 50);
+        OVERWORLD_DIAMOND_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk of diamond ore").defineInRange("overworldDiamondChunkAmount", 6, 0, 50);
+        DEEPSLATE_DIAMOND_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of diamond ore in deepslate").defineInRange("deepslateDiamondVeinsize", 3, 0, 50);
+        DEEPSLATE_DIAMOND_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk of diamond ore in deepslate").defineInRange("deepslateDiamondChunkAmount", 4, 0, 50);
+
+        OVERWORLD_COAL_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of all coal ores").defineInRange("overworldCoalVeinsize", 14, 0, 50);
+        OVERWORLD_COAL_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk").defineInRange("overworldCoalChunkAmount", 18, 0, 50);
+        DEEPSLATE_COAL_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of all coal ores in deepslate").defineInRange("deepslateCoalVeinsize", 14, 0, 50);
+        DEEPSLATE_COAL_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk in deepslate").defineInRange("deepslateCoalChunkAmount", 10, 0, 50);
 
         NETHER_GOLD_ORE_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of nether gold ore").defineInRange("netherGoldVeinsize", 5, 0, 50);
-        NETHER_GOLD_ORE_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk").defineInRange("netherGoldVeinsize", 10, 0, 50);
-        NETHER_QUARTZ_ORE_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of nether quartz ore").defineInRange("netherGoldVeinsize", 10, 0, 50);
-        NETHER_QUARTZ_ORE_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk").defineInRange("netherGoldVeinsize", 16, 0, 50);
+        NETHER_GOLD_ORE_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk").defineInRange("netherGoldChunkAmount", 10, 0, 50);
+        NETHER_QUARTZ_ORE_VEINSIZE = COMMON_BUILDER.comment("Default veinsize of nether quartz ore").defineInRange("netherQuartzVeinsize", 10, 0, 50);
+        NETHER_QUARTZ_ORE_AMOUNT = COMMON_BUILDER.comment("Default maximum amount of generations per chunk").defineInRange("netherQuartzChunkAmount", 16, 0, 50);
     }
 }
