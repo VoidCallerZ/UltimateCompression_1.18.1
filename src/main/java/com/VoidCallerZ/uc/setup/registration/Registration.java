e458f713da09736174aaba297ead474daffc1621
VoidCallerZ <rick.rasenberg@xs4all.nl>
2022-06-08 17:13:56 +0200

    Added new textures

--- src/main/java/com/VoidCallerZ/uc/setup/registration/Registration.java
+++ src/main/java/com/VoidCallerZ/uc/setup/registration/Registration.java
@@ -178,8 +178,8 @@ public class Registration
     public static final RegistryObject<Item> COMPRESSED_POLISHED_GRANITE_ITEM = fromBlock(COMPRESSED_POLISHED_GRANITE);
 
     //Smooth Blocks
-    public static final RegistryObject<Block> COMPRESSED_SMOOTH_STONE = BLOCKS.register("compressed_smooth_stone", () -> new Block(ORE_PROPERTIES));
     public static final RegistryObject<Block> COMPRESSED_SMOOTH_BASALT = BLOCKS.register("compressed_smooth_basalt", () -> new Block(ORE_PROPERTIES));
+    public static final RegistryObject<Block> COMPRESSED_SMOOTH_STONE = BLOCKS.register("compressed_smooth_stone", () -> new Block(ORE_PROPERTIES));
 
     //Nether Blocks
     public static final RegistryObject<Block> COMPRESSED_NETHER_QUARTZ_BLOCK = BLOCKS.register("compressed_nether_quartz_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.QUARTZ).requiresCorrectToolForDrops().strength(0.8F)));
