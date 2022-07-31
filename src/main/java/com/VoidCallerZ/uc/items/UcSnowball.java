1d4877e239b023cede0b478145a4ade816e0319c
VoidCallerZ <rick.rasenberg@xs4all.nl>
2022-07-31 21:41:35 +0200

    Updated to 1.19.1

--- src/main/java/com/VoidCallerZ/uc/items/UcSnowball.java
+++ src/main/java/com/VoidCallerZ/uc/items/UcSnowball.java
@@ -1,18 +1,12 @@
 package com.VoidCallerZ.uc.items;
 
 import com.VoidCallerZ.uc.setup.registration.ItemRegistration;
-import net.minecraft.core.particles.ItemParticleOption;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.projectile.Snowball;
-import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.entity.projectile.Snowball;;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -45,7 +39,6 @@ public class UcSnowball extends Snowball
     protected void onHit(HitResult result)
     {
         super.onHit(result);
-        System.out.println("onHit Reached");
         if (result.getType() == HitResult.Type.ENTITY)
         {
             this.onHitEntity((EntityHitResult) result);
