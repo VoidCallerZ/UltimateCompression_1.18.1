d1a1c628ceafbc67ba5bc8cacb537c4e3be131dd
VoidCallerZ <rick.rasenberg@xs4all.nl>
2022-08-29 19:41:20 +0200

    Also added abstract compressor block class

---
+++ src/main/java/com/VoidCallerZ/uc/blocks/compressor/CompressorBlock.java
@@ -0,0 +1,107 @@
+package com.VoidCallerZ.uc.blocks.compressor;
+
+import com.VoidCallerZ.uc.blocks.compressor.test.TestCompressorBlockEntity;
+import com.VoidCallerZ.uc.registration.BlockRegistration;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.event.level.NoteBlockEvent;
+import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class CompressorBlock extends BaseEntityBlock
+{
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final BooleanProperty LIT = BlockStateProperties.LIT;
+
+    public CompressorBlock(BlockBehaviour.Properties properties)
+    {
+        super(properties);
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH).setValue(LIT, Boolean.valueOf(false)));
+    }
+
+    private static final VoxelShape SHAPE = Shapes.block();
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
+    {
+        return SHAPE;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation)
+    {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror)
+    {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        builder.add(FACING, LIT);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) { return RenderShape.MODEL; }
+
+    @Override
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving)
+    {
+        if (state.getBlock() != newState.getBlock())
+        {
+            BlockEntity blockEntity = level.getBlockEntity(pos);
+            if (blockEntity instanceof TestCompressorBlockEntity)
+            {
+                ((TestCompressorBlockEntity) blockEntity).drops();
+            }
+        }
+        super.onRemove(state, level, pos, newState, isMoving);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
+    {
+        if (level.isClientSide)
+        {
+            return InteractionResult.SUCCESS;
+        }
+        else
+        {
+            this.openContainer(level, pos, player);
+            return InteractionResult.CONSUME;
+        }
+    }
+
+    protected abstract void openContainer(Level level, BlockPos pos, Player player);
+}
--- src/main/java/com/VoidCallerZ/uc/blocks/compressor/test/TestCompressorBlock.java
+++ src/main/java/com/VoidCallerZ/uc/blocks/compressor/test/TestCompressorBlock.java
@@ -1,123 +1,54 @@
 package com.VoidCallerZ.uc.blocks.compressor.test;
 
-import com.VoidCallerZ.uc.blocks.compressor.iron.IronCompressorBlockEntity;
+import com.VoidCallerZ.uc.blocks.compressor.CompressorBlock;
 import com.VoidCallerZ.uc.registration.BlockRegistration;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
-public class TestCompressorBlock extends BaseEntityBlock
+public class TestCompressorBlock extends CompressorBlock
 {
-    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-    public static final BooleanProperty LIT = BlockStateProperties.LIT;
-
     public TestCompressorBlock(BlockBehaviour.Properties properties)
     {
         super(properties);
-        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH).setValue(LIT, Boolean.valueOf(false)));
     }
 
-    private static final VoxelShape SHAPE = Shapes.block();
-
-    @Override
-    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
-    {
-        return SHAPE;
-    }
-
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext context)
-    {
-        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
-    }
-
-    @Override
-    public BlockState rotate(BlockState state, Rotation rotation)
-    {
-        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
-    }
-
-    @Override
-    public BlockState mirror(BlockState state, Mirror mirror)
-    {
-        return state.rotate(mirror.getRotation(state.getValue(FACING)));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
-    {
-        builder.add(FACING, LIT);
-    }
-
-    @Override
-    public RenderShape getRenderShape(BlockState state) { return RenderShape.MODEL; }
-
-    @Override
-    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving)
-    {
-        if (state.getBlock() != newState.getBlock())
-        {
-            BlockEntity blockEntity = level.getBlockEntity(pos);
-            if (blockEntity instanceof TestCompressorBlockEntity)
-            {
-                ((TestCompressorBlockEntity) blockEntity).drops();
-            }
-        }
-        super.onRemove(state, level, pos, newState, isMoving);
-    }
-
-    @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
-    {
-        if (!level.isClientSide())
-        {
-            BlockEntity entity = level.getBlockEntity(pos);
-            if (entity instanceof TestCompressorBlockEntity)
-            {
-                NetworkHooks.openScreen(((ServerPlayer) player), (TestCompressorBlockEntity)entity, pos);
-            }
-            else
-            {
-                throw new IllegalStateException("Our Container provider is missing!");
-            }
-        }
-
-        return InteractionResult.sidedSuccess(level.isClientSide());
-    }
-
-    @Nullable
-    @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state)
     {
         return new TestCompressorBlockEntity(pos, state);
     }
 
     @Nullable
-    @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType)
+    public <T extends BlockEntity>BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType)
     {
         return createTickerHelper(blockEntityType, BlockRegistration.TEST_COMPRESSOR_BLOCK_ENTITY.get(), TestCompressorBlockEntity::tick);
+    }
+
+    @Override
+    protected void openContainer(Level level, BlockPos pos, Player player)
+    {
+        BlockEntity entity = level.getBlockEntity(pos);
+        if (entity instanceof TestCompressorBlockEntity)
+        {
+            NetworkHooks.openScreen(((ServerPlayer) player), (TestCompressorBlockEntity) entity, pos);
+        }
+        else
+        {
+            throw new IllegalStateException("Our container provider is missing");
+        }
+    }
+
+    public void effectTick(BlockState state, Level level, BlockPos pos, RandomSource random)
+    {
+        //For sound and particle effects
     }
 }
